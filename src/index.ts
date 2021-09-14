ddc6e9ffdaf1764cf1456d9f2d08970bed9df3bd
black996 <bl4ck.96@yandex.com>
2021-09-14 17:04:09 +0300

    added getAll method to Attributes and save method to User class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 2 });
+const user = new User({ id: 2, name: "newer name", age: 0 });
 
-user.on("change", () => {
+user.on("save", () => {
   console.log(user);
 });
 
-user.fetch();
+user.save();
