195157bf15c2d7461e049bebc422fb455ddf06ab
black996 <bl4ck.96@yandex.com>
2021-09-14 16:53:26 +0300

    implemented fetch method on User class to fetch and sync a user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 import { Attributes } from "./Attributes";
+import { AxiosResponse } from "axios";
 
 export interface UserProps {
   id?: number;
@@ -34,5 +35,15 @@ export class User {
   set(update: UserProps) {
     this.attributes.set(update);
     this.events.trigger("change");
+  }
+
+  fetch(): void {
+    const id = this.get("id");
+    if (typeof id != "number") {
+      throw new Error("Cannot fetch without an id");
+    }
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
   }
 }
