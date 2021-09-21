c5ba69d222c66b4965500900c0a4b305b3b11d75
black996 <bl4ck.96@yandex.com>
2021-09-22 01:12:13 +0300

    Modified class Sync name, added static method buildUser that generates a User class instance

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 2, name: "newer name", age: 0 });
+// const user = new User({ id: 2, name: "newer name", age: 0 });
 
-user.on("save", () => {
+// user.on("save", () => {
+//   console.log(user);
+// });
+
+// user.save();
+
+const user = User.buildUser({ id: 2 });
+
+user.on("change", () => {
   console.log(user);
 });
 
-user.save();
+user.fetch();
