b13f4e042413726dbb5263f8ea06e79e77947089
black996 <bl4ck.96@yandex.com>
2021-09-22 22:54:12 +0300

    added fetch method to Collection class

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
-import { User } from "./models/User";
+import { Collection } from "./models/Collection";
 
-// const user = new User({ id: 2, name: "newer name", age: 0 });
+const collection = new Collection("http://localhost:3000/users");
 
-// user.on("save", () => {
-//   console.log(user);
-// });
-
-// user.save();
-
-const user = User.buildUser({ id: 2 });
-
-user.on("change", () => {
-  console.log(user);
+collection.on("change", () => {
+  console.log(collection);
 });
 
-user.fetch();
+collection.fetch();
