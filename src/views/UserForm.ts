d5e0c971b595f0615b0e3b5b75dd43381fc97241
black996 <bl4ck.96@yandex.com>
2021-09-28 00:49:38 +0300

    add bindModel method and execute it in the constructor

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -6,6 +6,10 @@ interface EventsMap {
 
 export class UserForm {
   constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
     this.model.on("change", () => this.render());
   }
 
