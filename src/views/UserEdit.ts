9a368416de4b58cbaf5f419fc2aca6753d471485
black996 <bl4ck.96@yandex.com>
2021-09-29 20:24:22 +0300

    implemented rendering nested elements on the DOM

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -1,5 +1,7 @@
 import { View, RegionsMap } from "./View";
 import { User, UserProps } from "../models/User";
+import { UserForm } from "./UserForm";
+import { UserShow } from "./UserShow";
 
 export class UserEdit extends View<User, UserProps> {
   regionsMap(): RegionsMap {
@@ -8,6 +10,14 @@ export class UserEdit extends View<User, UserProps> {
       userForm: "#user-form",
     };
   }
+
+  onRender() {
+    const userShow = new UserShow(this.regions.userShow, this.model);
+    userShow.render();
+    const userForm = new UserForm(this.regions.userForm, this.model);
+    userForm.render();
+  }
+
   template(): string {
     return `
       <div>
